d67284e1fa60518e270b5e82d7aac9f64847d35c
Sotaro Tommykawa <tommysouta@gmail.com>
2021-05-25 19:44:06 +0900

    🚑️ リンクのパスを修正

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -13,11 +13,12 @@
 </script>
 
 <script lang="ts">
+  import { base } from '$app/paths';
   export let articles: { title: string; href: string; }[];
 </script>
 
 <main>
   {#each articles as article}
-    <h3><a href="/{article.href}">{article.title}</a></h3>
+    <h3><a href="{base}/{article.href}">{article.title}</a></h3>
   {/each}
 </main>
