9ab5c92ef3589c4f827227628795df62f245e226
Sotaro Tommykawa <tommysouta@gmail.com>
2021-05-26 01:33:11 +0900

    🥅 エラーハンドリングを追加

--- src/routes/[slug].json.ts
+++
@@ -1,9 +0,0 @@
-import { markdownParser } from "$lib/markdown";
-import type { RequestHandler } from "@sveltejs/kit";
-
-export const get: RequestHandler = ({ params }) => {
-  const article = markdownParser(`./src/docs/${params.slug}.md`);
-  return {
-    body: JSON.stringify(article),
-  };
-};
